23ec70f915d6d365c70a86ddb6cb90e3dc764549
bobratu <nw1114257@student.francistuttle.edu>
2025-09-23 10:25:25 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import * as THREE from 'three';
 import Core from './core/core.js';
 import Controls from './core/controls.js';
-
+import UniqueArray from './core./unique_array.js'
 
 const canvas = document.querySelector('#c');
 
-const bruh = new Core(canvas);
-const controls = new Controls();
 
-function help() {
-				console.log(controls.mouse);
-}
-
-
-bruh.loopFunctions.push(help);
+const test = UniqueArray();
+const bruh = test.add("3")
